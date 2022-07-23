4ac8adff6a0d5f6727efbfefa2b32495c0c4077e
Reward Edoyugbo <rewardraj@gmail.com>
2022-07-23 16:33:29 +0200

    small cleanups

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ new fullpage('#fullpage', {
     autoscrolling: true,
     scrollHorizontally: true,
     navigation: true,
-    onLeave: (origin, destination) => {
-        const section = destination.item;
-        const title = section.querySelector('h1');
-        gsap.to("title", 0.5, { y: "50", opacity: 0, rotation: 360, scale:0.5});
-    }
-
+    navigationTooltips: ['Home', 'About', 'Stack', 'Contact']
 });
 
